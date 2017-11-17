ec16b2586f2b2a4815dbdd222c748f3544774aba
JonatanLundqvist <jonatanlundqvist9999@gmail.com>
2017-11-17 10:39:40 +0100

    första commit

---
+++ index.ts
@@ -0,0 +1,16 @@
+module com.teknikgymnasiet.programmering {
+
+    export class Lektion2{
+        constructor () {
+            console.log("Hello World!");
+        }
+    }
+}
+
+let instans : any = new com.teknikgymnasiet.programmering.Lektion2();
+
+
+
+
+
+
